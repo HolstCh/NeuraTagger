4e0413588825b0b6b64440b6bb294195f4095efd
HolstCh <chad.holst@Hotmail.com>
2024-02-27 12:09:24 -0700

    reduces training loss and increases training set accuracy

--- IntentClassifier.py
+++ IntentClassifier.py
@@ -9,14 +9,17 @@ from torch.utils.data import DataLoader, Dataset
 from torch.nn.utils.rnn import pad_sequence
 from torchtext.vocab import GloVe
 # nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+
+model_saved = False
 
 
 class IntentModelArchitecture(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, pretrained_embeddings=None):
         super(IntentModelArchitecture, self).__init__()
-        print(pretrained_embeddings)
         # embedding layer is first layer since need to covert text to numerical format, reduce dimensionality of input data, and find semantics
         if pretrained_embeddings is not None:
             self.embedding_layer = nn.Embedding.from_pretrained(pretrained_embeddings)
@@ -26,16 +29,21 @@ class IntentModelArchitecture(nn.Module):
         # LSTM layer is a hidden layer that allows model to learn long-term dependencies in sequential data and mitigates vanishing gradient problem
         self.lstm_layer = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
 
-        # Linear layer
+        # dropout layer with p=0.5
+        self.dropout = nn.Dropout(p=0.1)
+
+        # linear layer
         self.linear_layer = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, text):
         # convert input text to dense word embeddings
-        embedded = self.embedding_layer(text)
+        embedded = self.embedding_layer(text.long())
         # feed word embeddings into LSTM and return output for each timestep and ignored final hidden/cell states
         lstm_out, _ = self.lstm_layer(embedded)
         # take all elements from 1D, take last element in 2D, and take all elements from 3D
         lstm_out = lstm_out[:, -1, :]
+        # apply dropout after LSTM layer
+        lstm_out = self.dropout(lstm_out)
         # final linear layer that acts as classifier
         output = self.linear_layer(lstm_out)
         return output
@@ -56,8 +64,11 @@ class IntentModelTrainer:
         print(len(train_data_loader))
         # set model to training mode (layers behave differently compared to validate mode)
         self.model.train()
-        # keep track of total loss
+        # keep track of total loss and correct predictions
         total_loss = 0
+        correct_predictions = 0
+        total_samples = 0
+
         # iterate through training data in batches (batches of features/labels)
         for texts, intents in train_data_loader:
             # move features/labels to CPU device
@@ -66,31 +77,44 @@ class IntentModelTrainer:
             self.optimizer.zero_grad()
             # feed features into model to get labels
             outputs = self.model(texts)
-            # compute the loss using loss function (cross entropy)
+            # compute the loss using the loss function (cross entropy)
             loss = self.criterion(outputs, intents.long())
-            # performs backpropogation to compute gradients with respect to model's parameters
+            # performs backpropagation to compute gradients with respect to model's parameters
             loss.backward()
-            # update step which updates paramaters using gradients computed in backward pass
+            # update step which updates parameters using gradients computed in backward pass
             self.optimizer.step()
-            # adds loss value of current pass to total loss value (converted to Python number)
+            # adds loss value of the current pass to total loss value (converted to Python number)
             total_loss += loss.item()
-            # returns average loss per batch
-        return total_loss / len(train_data_loader)
 
-    def evaluate(self, val_loader):
+            # calculate accuracy
+            _, predicted = torch.max(outputs, 1)
+            correct_predictions += (predicted == intents).sum().item()
+            total_samples += intents.size(0)
+
+        # calculate accuracy
+        accuracy = correct_predictions / total_samples
+        # return average loss and accuracy per batch
+        return total_loss / len(train_data_loader), accuracy
+
+    def validate(self, val_loader):
+        # set the model to evaluation mode
         self.model.eval()
+        # lists for preds and labels
         all_preds, all_labels = [], []
-
+        # disable gradient computations during validation
         with torch.no_grad():
-            for inputs, labels in val_loader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-
-                outputs = self.model(inputs)
+            # loop over batches of texts and intents from the validation loader
+            for texts, intents in val_loader:
+                # move features/labels to CPU device
+                texts, intents = texts.to(self.device), intents.to(self.device)
+                # feed features as forward pass in model to get predictions
+                outputs = self.model(texts)
+                # extract predicted classes using index of max value of dim=1
                 _, preds = torch.max(outputs, 1)
-
+                # extend lists with current batches preds and labels
                 all_preds.extend(preds.cpu().numpy())
-                all_labels.extend(labels.cpu().numpy())
-
+                all_labels.extend(intents.cpu().numpy())
+        # compute accuracy of model
         accuracy = accuracy_score(all_labels, all_preds)
         return accuracy
 
@@ -117,7 +141,7 @@ class IntentModelDataset(Dataset):
         text = self.dataframe.iloc[i]['text']
         intent = self.dataframe.iloc[i]['intent']
         # convert each word in the text to its corresponding index in vocab
-        text_indices = [self.vocabulary.index(word) for word in text]
+        text_indices = [self.vocabulary.index(word) for word in text if word in self.vocabulary]
         # convert intent into numeric format
         intent = self.label_encoder.transform([intent])[0]
 
@@ -141,6 +165,7 @@ with open('data_full.json', 'r') as fp:
 for key in data_full.keys():
     if key == "oos_val":
         oos_val = pd.DataFrame(data_full[key], columns=['text', 'intent'])
+        oos_val = oos_val.sample(n=20)
     elif key == "val":
         is_val = pd.DataFrame(data_full[key], columns=['text', 'intent'])
     elif key == "train":
@@ -160,32 +185,33 @@ test_df = pd.concat([is_test, oos_test])
 # preprocess training data
 vocab = set()
 train_df['text'] = train_df['text'].apply(lambda x: x.lower())  # apply lowercase to all text inputs
-# train_df['text'] = train_df['text'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))  # remove punctuation
-train_df['text'] = train_df['text'].apply(lambda x: nltk.tokenize.word_tokenize(x))  # tokenize text
-# stop_words = set(nltk.corpus.stopwords.words('english')) # get set of stopwords
-# train_df['text'] = train_df['text'].apply(lambda x: [word for word in x if word not in stop_words]) # remove stopwords
-# stemmer = nltk.stem.PorterStemmer()
-# train_df['text'] = train_df['text'].apply(lambda x: [stemmer.stem(word) for word in x])
-# lematizer = nltk.stem.WordNetLemmatizer()
-# train_df['text'] = train_df['text'].apply(lambda x: [lematizer.lemmatize(word) for word in x])
+train_df['text'] = train_df['text'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))  # remove punctuation
+train_df['text'] = train_df['text'].apply(lambda x: nltk.tokenize.word_tokenize(x))
+stop_words = set(nltk.corpus.stopwords.words('english')) # get set of stopwords
+train_df['text'] = train_df['text'].apply(lambda x: [word for word in x if word not in stop_words]) # remove stopwords
+#stemmer = nltk.stem.PorterStemmer()
+#train_df['text'] = train_df['text'].apply(lambda x: [stemmer.stem(word) for word in x])
+lematizer = nltk.stem.WordNetLemmatizer()
+train_df['text'] = train_df['text'].apply(lambda x: [lematizer.lemmatize(word) for word in x])
 
 # add each token to the set
 for tokens in train_df['text']:
     vocab.update(tokens)
 
+print(vocab)
 # instantiate CustomDataset objects to be loaded
 train_dataset = IntentModelDataset(train_df, vocab)
 validate_dataset = IntentModelDataset(validate_df, vocab)
 test_dataset = IntentModelDataset(test_df, vocab)
 
 # instantiate DataLoader objects to shuffle data to reduce bias, speed training process with batch size, padding, etc.
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, collate_fn=padding_fn)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=padding_fn)
 validate_loader = DataLoader(validate_dataset, batch_size=32, shuffle=True, collate_fn=padding_fn)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True, collate_fn=padding_fn)
 
 # define hyperparameters for IntentModelArchitecture object and instantiate pre-trained word embedding
 vocab_size = len(vocab)
-embedding_dim = 100
+embedding_dim = 300
 hidden_dim = 500
 output_dim = 151  # 150 in scope intent class and 1 oos intent class
 glove = GloVe(name='6B', dim=embedding_dim)
@@ -195,13 +221,28 @@ model = IntentModelArchitecture(vocab_size, embedding_dim, hidden_dim, output_di
 
 # define the loss function and optimization algo
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.005)
 
-# instantiate IntentModelTrainer to start training the model
-trainer = IntentModelTrainer(model, criterion, optimizer)
-avg_loss = trainer.train(train_loader)
-print(avg_loss)
-# need to improve avg_loss from 4.8148555311106020
+# instantiate IntentModelTrainer to start training the model (7/10 epoch seems best so far with avg loss per batch at 0.049522,
+# next run had 8/10 epoch with avg loss per batch at 0.063428 ... also tuned learning rate to 0.005 from 0.001)
+if not model_saved:
+    trainer = IntentModelTrainer(model, criterion, optimizer)
+    epochs = 2
+    for epoch in range(epochs):
+        avg_loss, accuracy = trainer.train(train_loader)
+        print(
+            f'Epoch {epoch + 1}/{epochs}, Average Loss: {avg_loss}, Training Set Accuracy: {accuracy}')  # need to improve avg_loss from 4.8148555311106020
+
+    val_accuracy = trainer.validate(validate_loader)
+    print(f'Validation Set Accuracy: {val_accuracy}')
+    # save the trainer object to be loaded later
+    torch.save(trainer, 'intent_model_trainer.pth')
+
+if model_saved:
+    # Load the saved trainer object
+    trainer = torch.load('intent_model_trainer.pth')
+    val_accuracy = trainer.validate(validate_loader)
+    print(f'Validation Set Accuracy: {val_accuracy}')
 
 # Example Usage:
 # Initialize your dataset and DataLoader, and load pre-trained embeddings (e.g., GloVe)
